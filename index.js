63999a8974b0773f91c92b6140d33404a5da6eb7
Joao Paulo <joaopaulo_lemos07@hotmail.com>
2022-01-06 21:05:02 -0300

    config database

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars');
+
+//config template engine
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
